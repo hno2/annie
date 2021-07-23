d62a26555d3cc26fe6cf4a8d743ffada0c723f4f
Simon Klug <github@simonklug.de>
2021-07-23 10:03:52 +0200

    Reviewing Notebooks Works without Assessment

--- annie/blueprints/eval/views.py
+++ annie/blueprints/eval/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, current_app
 
 # from annie.blueprints.eval.tasks import get_preds
 from annie.blueprints.eval.steve import static_code_check, convert_to_html
+from nbformat.reader import NotJSONError
 
 eval = Blueprint("eval", __name__, template_folder="templates")
 
@@ -11,7 +12,10 @@ def home(filepath):
     with open("uploads/" + filepath, "r") as file:
         content = file.read()
     file_data = {}
-    file_data["html"] = convert_to_html(content)
+    try:
+        file_data["html"] = convert_to_html(content)
+    except NotJSONError:
+        return "Something went wrong while formatting"
     if filepath.split(".")[1] == "py":
         score, msgs = static_code_check(filepath)
         file_data["static"] = {"score": score, "msgs": msgs}
